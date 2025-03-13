4977f78047c2d4a7ecb40e17c7731ddd67f6886e
Ivan Petrov <ivan.p.petrov02@gmail.com>
2025-03-13 12:17:36 +0200

    remove Angular as it was not tested

--- Angular_test/angular-test/src/main.ts
+++
@@ -1,7 +0,0 @@
-import "zone.js";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
-import { AppModule } from "./app/app.module.ts";
-
-platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch((err) => console.error(err));
